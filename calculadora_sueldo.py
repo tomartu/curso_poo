a5a4e0acba3b3fe7e27a7d1a637884771feaf50f
nem <yonicomali@gmail.com>
2023-09-27 19:47:11 -0300

    Adecuo nombres de variables

--- calculadora_sueldo.py
+++ calculadora_sueldo.py
@@ -1,15 +1,15 @@
-Maquinista: float = 211203.00
-Recibidor: float = 211203.00
-Comida_viatico: float = 5383.63
+maquinista: float = 211203.00
+recibidor: float = 211203.00
+comida_viatico: float = 5383.63
 
-categoria = input("ingrese si categoria Maquinista (M) o Recibidor (R): ")
+categoria = input("ingrese si categoria Maquinista (M) o recibidor (R): ")
 Dias = input("cuantos dias laborables tuvo el mes: ")
-Dias_laborables = int(Dias) * Comida_viatico
+Dias_laborables = int(Dias) * comida_viatico
 
 if categoria == "M":
-    print("Sueldo es de :"+str(Maquinista))
+    print("Sueldo es de :"+str(maquinista))
     print("Sueldo viatico y comida es de : "+str(Dias_laborables))
 elif categoria == "R":
-    print("Sueldo es de :"+str(Recibidor))
+    print("Sueldo es de :"+str(recibidor))
     print("Sueldo viatico y comida es de : "+str(Dias_laborables))
     
