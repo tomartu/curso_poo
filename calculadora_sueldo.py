b9d988340ed9ab2977afc43b3be1769eefe3b009
nem <yonicomali@gmail.com>
2023-09-27 19:42:09 -0300

    Cambio a mayusculas

--- calculadora_sueldo.py
+++ calculadora_sueldo.py
@@ -3,12 +3,13 @@ Recibidor: float = 211203.00
 Comida_viatico: float = 5383.63
 
 categoria = input("ingrese si categoria Maquinista (M) o Recibidor (R): ")
-Dias=input("cuantos dias laborables tuvo el mes: ")
+Dias = input("cuantos dias laborables tuvo el mes: ")
 Dias_laborables = int(Dias) * Comida_viatico
 
 if categoria == "M":
-    print("sueldo es de :"+str(Maquinista))
-    print("sueldo viatico y comida es de : "+str(Dias_laborables))
+    print("Sueldo es de :"+str(Maquinista))
+    print("Sueldo viatico y comida es de : "+str(Dias_laborables))
 elif categoria == "R":
-    print("sueldo es de :"+str(Recibidor))
-    print("sueldo viatico y comida es de : "+str(Dias_laborables))
+    print("Sueldo es de :"+str(Recibidor))
+    print("Sueldo viatico y comida es de : "+str(Dias_laborables))
+    
