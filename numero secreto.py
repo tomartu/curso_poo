99918311ad7d76ae8cabdeb51e8dad31946dd5b4
Pablo <pablo106388@gmail.com>
2023-10-18 20:01:27 -0300

    adivina el numero

--- numero secreto.py
+++ numero secreto.py
@@ -8,11 +8,14 @@ while True:
     except:
         print("no es un numero")
         continue
+    if numero == numero_secreto:
+          print("adivinaste: ",numero_secreto)
+    
     if numero  != numero_secreto:
         if numero > numero_secreto:
             print("el numero es menor")
 
         elif numero < numero_secreto:
             print("el numero es mayor")
-        else:
-            print("adivinaste: ",numero_secreto)
+            
+       
